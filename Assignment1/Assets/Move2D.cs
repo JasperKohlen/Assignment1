ccbb8abab240fbd3bb1f00dccbdd86cefe7b425e
Jasper Kohlen <386329@student.fontys.nl>
2020-03-19 18:25:31 +0100

    Animation for idle and jump

---
+++ Assignment1/Assets/Move2D.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        if (isGrounded == true)
+        {
+            animator.SetBool("IsFlying", false);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            animator.SetBool("IsFlying", true);
+        }
+    }
+}
